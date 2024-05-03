e6eb6e7123fbf7c8fc8fcfbeb8970c5b8a9e9a06
viniborges99 <borgesvinicius27@gmail.com>
2024-05-03 19:03:28 -0300

    corrigindo a responsividade

--- src/components/Products/styles.js
+++ src/components/Products/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const ProductsContainer = styled.section`
   padding: 140px 60px 50px;
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
   gap: 30px;
 `;
 
